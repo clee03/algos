ffb690ac9468a72b3c33d9037f3da0c2b0a01b59
clee03 <cathylee03@gmail.com>
2022-03-11 16:43:25 -0700

    Setting up project

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 Using my favorite (only) algorithm textbook to better understand algorithms and practice Python. Hooray.
 - Insertion sort
 
+## Insertion Sort
+
+
+
 ## References
 TODO Textbook reference
 TODO Python reference
--- insertion-sort.py
+++ insertion-sort.py
@@ -1,4 +1,3 @@
-
 def main():
     print("Insertion Sort")
     a1 = [5,2,4,6,1,3]
@@ -10,13 +9,11 @@ def insertion_sort(a):
     for j in range(1, len(a)):
         key = a[j]
         i = j - 1
+        # swap
         while ((i >= 0) and (a[i] > key)):
-            print("compare " + str(a[i]) + " is > " + str(key))
             a[i + 1] = a[i]
-            print("move key " + str(key) + " to index " + str(i))
             i = i - 1
         a[i + 1] = key
-        print("round " + str(j) + ": " + str(a))
 
     print("final: " + str(a))
 
